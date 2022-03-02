80ce392326c88b76483b5d9fb2cefc97c44ff6bc
Luke Pascale <lpluke5@gmail.com>
2022-03-02 15:10:46 -0700

    refactor: Remove unneeded codelines

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,14 +1,8 @@
-require 'pry'
 require 'csv'
 require_relative 'repository'
 require_relative 'invoice_item'
 
 class InvoiceItemRepository < Repository
-
-#  def inspect
-#    "#<\#{self.class} \#{@invoice_items.size} rows>"
-#  end
-
   attr_reader :invoice_items
 
   def initialize(data)
@@ -18,22 +12,6 @@ class InvoiceItemRepository < Repository
     end
     super(@invoice_items)
   end
-
-  # def all
-  #   @invoice_items
-  # end
-  #
-  # def find_by_id(id)
-  #   @invoice_items.find {|item| item.id == id}
-  # end
-  #
-  # def find_all_by_item_id(item_id)
-  #   @invoice_items.find_all {|item| item.item_id == item_id}
-  # end
-
-  # def find_all_by_invoice_id(invoice_id)
-  #   @invoice_items.find_all {|item| item.invoice_id == invoice_id}
-  # end
 
   def create(attributes)
     attributes[:id] = @invoice_items.last.id + 1
@@ -47,17 +25,13 @@ class InvoiceItemRepository < Repository
   def update(id, attributes)
     item_to_update = find_by_id(id)
     if item_to_update != nil
-        attributes.each do |key, value|
-          if ![:id, :item_id, :invoice_id, :created_at].include?(key)
-            item_to_update.info[key.to_sym] = value
-            item_to_update.info[:updated_at] = (Time.now + 1).to_s
-          end
+      attributes.each do |key, value|
+        if ![:id, :item_id, :invoice_id, :created_at].include?(key)
+          item_to_update.info[key.to_sym] = value
+          item_to_update.info[:updated_at] = (Time.now + 1).to_s
         end
+      end
     end
     item_to_update
   end
-
-  # def delete(id)
-  #   @invoice_items.delete(find_by_id(id)) if find_by_id(id) != nil
-  #   end
 end
